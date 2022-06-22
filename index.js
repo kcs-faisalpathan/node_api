ae6ee13c8dd18178e1b773dc6628bd7d0c04ad31
Faisal Pathan <fpathan@ashleyfurniture.com>
2022-06-22 06:55:38 +0530

    Simple API file

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+app.get("/healthcheck", (req, res, next) => {
+    res.json("Your API is healthy!");
+   });
+
+
+   app.get("/detail", (req, res, next) => {
+    res.json("Sample API V1.0.0");
+   });
